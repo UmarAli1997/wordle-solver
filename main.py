7ae308b97e77e800b4ea6de5346e68bb33580172
Umar Ali <umarali1997@gmail.com>
2023-01-20 18:27:33 +0000

    Inital Commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# Wordle Solver
+
+def wordle_solver():
+    
+    return
+
+def load_words():
+    # Define list to store 5 letter words
+    word_list = []
+
+    # Open file and read words into a list
+    with open("word_files/valid-wordle-words.txt", "r") as wordFile:
+        for word in wordFile:
+            word = word.replace("\n", "")
+            word_list.append(word)
+
+    return
+
+def filter_words():
+
+    
+    return
+
+load_words()

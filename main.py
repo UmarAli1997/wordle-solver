f9aa1a00b85c3d949425a41a00ffab04cb6ee78d
Umar Ali <umarali1997@gmail.com>
2023-01-20 18:44:23 +0000

    Added optimal guesses from MIT paper and started work on filter words function

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 # Wordle Solver
 
 def wordle_solver():
-    
+
+    word_list = get_words("word_files/valid-wordle-words.txt")
+
+    # Guesses taken from MITs paper
+    # https://auction-upload-files.s3.amazonaws.com/Wordle_Paper_Final.pdf
+    optimal_guesses = ["salet", "reast", "trace", "crate", "slate"]
     return
 
-def load_words():
+def get_words(file):
     # Define list to store 5 letter words
     word_list = []
 
     # Open file and read words into a list
-    with open("word_files/valid-wordle-words.txt", "r") as wordFile:
+    with open(file, "r") as wordFile:
         for word in wordFile:
             word = word.replace("\n", "")
             word_list.append(word)
 
+    return word_list
+
+def filter_words(guess_word, guess_result, word_list):
+
+    for result, index in enumerate(guess_result):
+        if result == "x" and word_list[]
     return
-
-def filter_words():
-
-    
-    return
-
-load_words()
